3646d4ae00da44c462ae2000cfca254644bba006
Gabriel Caires <gabriel_caires@acaciaconsultoria.com.br>
2021-05-31 09:14:19 -0300

    adicionado botões, estilos, barra de life

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -1,15 +1,24 @@
 new Vue({
   el: '#app',
   data: {
-    
+    running: false,
+    playerLife: 0,
+    monsterLife: 10,
   },
   computed: {
-
+    hasResult() {
+      return this.playerLife == 0 || this.monsterLife == 0;
+    },
+    isRunning() {
+      return this.running;
+    }
   },
   methods: {
-
+    updateStatusRunning() {
+      this.running = !this.running;
+    }
   },
   watch: {
 
   },
-}),
+});
