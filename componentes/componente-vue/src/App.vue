bd925e67cf9f008157b5bf2171d1c07c18e0f4f7
Gabriel Caires <gabriel_caires@acaciaconsultoria.com.br>
2021-06-10 09:46:28 -0300

    módulo componentes

---
+++ componentes/componente-vue/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div>Contadores</div>
+    <app-contadores />
+    <div class="contador">
+      <button>?</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+</style>
---
+++ componentes/componente-vue/src/Contador.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="contador">
+    <span>{{ contadorr }}</span>
+    <button @click="adicionar">+</button>
+    <button @click="subtrair">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contadorr: 0
+    };
+  },
+  methods: {
+    adicionar() {
+      this.contadorr++;
+    },
+    subtrair() {
+      this.contadorr--;
+    },
+  },
+}
+</script>
+
+<style scoped>
+  .contador span {
+    border-bottom: 1px solid #CCC;
+    height: 30px;
+    padding: 5px 25px;
+  }
+
+  .contador button {
+    height: 30px;
+    width: 30px;
+    border-radius: 15px;
+    background-color: coral;
+    outline: none;
+    margin-left: 10px;
+    color: #fff;
+  }
+</style>
