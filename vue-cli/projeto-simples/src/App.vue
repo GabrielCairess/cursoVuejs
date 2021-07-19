8c233aa7d56c9caa2ea11354fb670f94d8cbb1e3
Gabriel Caires <gabriel_caires@acaciaconsultoria.com.br>
2021-07-19 14:31:08 -0300

    adicionado modulos de componentes

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    {{ teste }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: function() {
+      return { teste : 'vrau' };
+    },
+}
+</script>
+
+<style>
+</style>
